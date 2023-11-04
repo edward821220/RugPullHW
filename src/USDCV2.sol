0f51c1bcc1c55388563c1d32461a27cb07bc90a5
Edward <edward821220@gmail.com>
2023-11-04 14:40:27 +0800

    let USDCV2 inherit IERC20

--- src/USDCV2.sol
+++ src/USDCV2.sol
@@ -3,6 +3,18 @@ pragma solidity ^0.8.17;
 
 import {MerkleProof} from "./utils/MerkleProof.sol";
 
+interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
 // 複製 USDC Logic Contract 及所有繼承合的狀態變數，讓順序保持一致
 contract USDCStorage {
     address internal _owner;
@@ -28,7 +40,7 @@ contract USDCStorage {
     uint8 internal _initializedVersion;
 }
 
-contract USDCV2 is USDCStorage {
+contract USDCV2 is USDCStorage, IERC20 {
     bytes32 private constant _ROOT_SLOT = keccak256("usdcv2.merkleRoot");
 
     modifier onlyWhitelisted(bytes32[] memory _merkleProof, address _who) {
@@ -53,9 +65,20 @@ contract USDCV2 is USDCStorage {
         return balances[_who];
     }
 
-    function totalSupply() public view returns (uint256) {
+    function totalSupply() external view returns (uint256) {
         return totalSupply_;
     }
+
+    function allowance(address owner, address spender) external view returns (uint256) {
+        return allowed[owner][spender];
+    }
+
+    // 不讓用戶有原本 ERC20 的 function 功能所以這三個故意不實作
+    function transfer(address to, uint256 value) external returns (bool) {}
+
+    function approve(address spender, uint256 value) external returns (bool) {}
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool) {}
 
     function setRoot(bytes32 _merkleRoot) public {
         _setSlot(_ROOT_SLOT, _merkleRoot);
